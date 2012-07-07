60fde1a43a207c83906f148d3594aa88782de43c
Adam Awan <adamja1@gmail.com>
2012-07-07 21:09:26 +0100

    Adapting to handle PyScss not taking an outfile argument

--- pipeline_compass/compiler.py
+++ pipeline_compass/compiler.py
@@ -21,4 +21,7 @@ class CompassCompiler(CompilerBase):
   def compile_file(self, infile, outfile, outdated=False, force=False):
     if not outdated and not force:
         return # No need to recompiled file
-    return scss.Scss().compile(infile,outfile)
+    out=open(outfile,'w')
+    compiled = scss.Scss().compile(infile)
+    out.write(compiled)
+    return compiled
